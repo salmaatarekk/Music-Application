d3e151dcbe5f7bb524a8b5365211c85cb5a139bd
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-09-27 20:26:48 +0200

    trying to Post a new User

--- music-app/reactFrontEnd/src/components/registerForm.jsx
+++ music-app/reactFrontEnd/src/components/registerForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import From from './common/form';
-
+import axios from 'axios';
 
 class RegisterForm extends From {
     state = {
@@ -12,18 +12,21 @@ class RegisterForm extends From {
     //     password : Joi.string().required().min(5).label('Password'),
     //     name: Joi.string().required().label('Name')
     // }
-   
+   handleRegister = (event) => {
+       event.preventDefault();
+       console.log(event.target.username.value) ;
+
+   };
     render() { 
         return (
 
             <div>
                 <h1>Register</h1>
-                <form onSubmit = {this.handleSubmit}>
+                <form onSubmit = {this.handleRegister}>
                 {this.renderInput('username', 'Username', true)}
                 {this.renderInput('password', 'Password', false, 'password')}
                 {this.renderInput('name', 'Name', false)}
-
-                {this.renderButton("Register")}
+                <button onClick = {this.handleRegister} className = "btn btn-primary" >Register</button>
                 </form>   
             </div>
 
