f92d3c6ef9b0baf23da0d669039907840df795a7
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-10-08 16:05:20 +0200

    fix bugs

--- music-app/reactFrontEnd/src/index.js
+++ music-app/reactFrontEnd/src/index.js
@@ -5,12 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-import MediaControlCard from './components/common/card';
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <MediaControlCard />
+    <App />
     </BrowserRouter>,
   document.getElementById('root')
 );
