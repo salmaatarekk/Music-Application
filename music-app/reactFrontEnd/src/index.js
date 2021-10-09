b3dd2f8b6f19bd75686cb880db82928035341975
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-10-09 19:14:47 +0200

    make card for songs using material ui

--- music-app/reactFrontEnd/src/index.js
+++ music-app/reactFrontEnd/src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import SongCard from './components/common/songCard';
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <SongCard />
     </BrowserRouter>,
   document.getElementById('root')
 );
